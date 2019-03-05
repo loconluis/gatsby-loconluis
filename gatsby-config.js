ed3711c64fbe7ce578ce65abd41147a67af8a10b
Luis Locon <luis.locon@gmail.com>
2019-03-04 22:59:57 -0600

    [Adding a eslintrc and change title on gastsby config]

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Luis Locon`,
+    description: `Project to show my resume, and a blog`,
+    author: `@loconluis`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
